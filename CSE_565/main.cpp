474e8a24b8c582f3b8eab09e349f825f19ff9081
Antonvinod <seliksamai@gmail.com>
2020-08-24 17:53:32 -0700

    Initial Commit

---
+++ CSE_565/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE_565
+//
+//  Created by samai on 8/24/20.
+//  Copyright © 2020 samai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
